6980971a36781af0c6885fb5cd9b36cff6f25a21
Shailesh Waman <shailesh.waman@gmail.com>
2018-05-07 03:09:54 +0200

    step3: generate report for count of parsed numbers

--- src/main/java/com/equalexperts/fb/FizzBuzz.java
+++ src/main/java/com/equalexperts/fb/FizzBuzz.java
@@ -1,15 +1,48 @@
 package com.equalexperts.fb;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FizzBuzz {
 
-    public String parseNumbers(int start, int end) {
+    private static final Map<String, Integer> COUNT_REPORT = new LinkedHashMap<>();
+
+    public FizzBuzz() {
+        COUNT_REPORT.put("fizz", 0);
+        COUNT_REPORT.put("buzz", 0);
+        COUNT_REPORT.put("fizzbuzz", 0);
+        COUNT_REPORT.put("lucky", 0);
+        COUNT_REPORT.put("integer", 0);
+    }
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.parseNumbersWithReport(1, 20));
+
+    }
+
+    public String parseNumbersWithReport(int start, int end) {
+        return parseNumbers(start, end) + " " + generateReport();
+    }
+
+    String parseNumbers(int start, int end) {
         final StringBuilder parsedResult = new StringBuilder();
         do {
-            parsedResult.append(numberToWord(start)).append(" ");
+            String numberToWord = numberToWord(start);
+            parsedResult.append(numberToWord).append(" ");
+            processReport(numberToWord);
             start++;
         } while (start <= end);
 
         return parsedResult.toString().trim();
+    }
+
+    private void processReport(String numberToWord) {
+        if (COUNT_REPORT.containsKey(numberToWord)) {
+            COUNT_REPORT.put(numberToWord, COUNT_REPORT.get(numberToWord) + 1);
+        } else {
+            COUNT_REPORT.put("integer", COUNT_REPORT.get("integer") + 1);
+        }
     }
 
     private String numberToWord(Integer number) {
@@ -35,4 +68,17 @@ public class FizzBuzz {
     private boolean containDigitThree(Integer number) {
         return number % 10 == 3;
     }
+
+    private String generateReport() {
+        StringBuilder reportToString = new StringBuilder();
+        for (Map.Entry<String, Integer> e : COUNT_REPORT.entrySet()) {
+            reportToString
+                    .append(" ")
+                    .append(e.getKey())
+                    .append(": ")
+                    .append(e.getValue());
+        }
+        return reportToString.toString().trim();
+    }
 }
+
--- src/test/java/com/equalexperts/fb/FizzBuzzTest.java
+++ src/test/java/com/equalexperts/fb/FizzBuzzTest.java
@@ -1,11 +1,17 @@
 package com.equalexperts.fb;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
-    private FizzBuzz subject = new FizzBuzz();
+    private FizzBuzz subject;
+
+    @Before
+    public void setUp() throws Exception {
+        subject = new FizzBuzz();
+    }
 
     @Test
     public void oneNumberRange_parseNumbers_number() {
@@ -97,5 +103,21 @@ public class FizzBuzzTest {
 
         // then
         assertEquals("lucky", parsedResult);
+    }
+
+    @Test
+    public void rangeFromOneToTwenty_parse_numbersWithFizzAndBuzzAndFizzBuzzWithReport() {
+        // given
+        int start = 1;
+        int end = 20;
+
+        // when
+        String parsedResult = subject.parseNumbersWithReport(start, end);
+
+        // then
+        assertEquals(
+                "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz fizz: 4 buzz: 3 fizzbuzz: 1 lucky: 2 integer: 10",
+                parsedResult
+        );
     }
 }
